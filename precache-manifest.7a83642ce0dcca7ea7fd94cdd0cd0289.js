f3ffd885c840564b399d1982a5c866332144fd1a
Mateusz Wojdalski <wojdalski.m@gmail.com>
2020-07-21 21:27:37 +0200

    Updates

---
+++ precache-manifest.7a83642ce0dcca7ea7fd94cdd0cd0289.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b3ce565f35edff9a77e4390cf9c6259",
+    "url": "/ToDo-List-React/index.html"
+  },
+  {
+    "revision": "421838efa124c07a7e32",
+    "url": "/ToDo-List-React/static/css/main.cbe0eec7.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/ToDo-List-React/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-List-React/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "421838efa124c07a7e32",
+    "url": "/ToDo-List-React/static/js/main.c03d2ed9.chunk.js"
+  },
+  {
+    "revision": "0a846f70feb29e27fe39",
+    "url": "/ToDo-List-React/static/js/runtime-main.394a1293.js"
+  }
+]);
